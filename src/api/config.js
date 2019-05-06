aa5a4baf01cc77c7c3f2fa84c5d4cf10cfe9efa9
Ariam Blanco Alzurutt <ariamxp.cl@gmail.com>
2019-05-05 20:10:59 -0400

    Initial Commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: '923f75959e1432641674ec1a9dafc00c',
+  secret: '2426ee85aae3269cb174bd341d9753c9',
+  registeredTo: 'ariamxp'
+}
+
+// Application name	platzimusic
+// API key	923f75959e1432641674ec1a9dafc00c
+// Shared secret	2426ee85aae3269cb174bd341d9753c9
+// Registered to	ariamxp
